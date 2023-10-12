bdfc36fd880f91580270ba60e264696fc46d99bf
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-12 23:39:51 +0900

    Add: Laravel/Inertia/Vue3の動作確認完了

---
+++ resources/js/Pages/hello-world.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            greeting: "Hello World!",
+            messeage: "Additional message",
+        };
+    },
+};
+</script>
+
+<template>
+    <p class="greeting">{{ greeting }}</p>
+    <p class="additional">{{ messeage }}</p>
+</template>
+
+<style>
+.greeting {
+    color: red;
+    font-weight: bold;
+}
+.additional {
+    font-weight: bold;
+}
+</style>
