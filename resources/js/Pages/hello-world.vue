440f005ac4eafc68c4b0151bb4252054e205959f
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-22 16:49:48 +0900

    SCSSのインストールと動作確認完了

--- resources/js/Pages/hello-world.vue
+++ resources/js/Pages/hello-world.vue
@@ -10,16 +10,29 @@ export default {
 </script>
 
 <template>
-    <p class="greeting">{{ greeting }}</p>
-    <p class="additional">{{ messeage }}</p>
+    <!-- <p class="greeting">{{ greeting }}</p>
+    <p class="additional">{{ messeage }}</p> -->
+    <div class="block">
+        <p class="greeting">{{ greeting }}</p>
+        <p class="message">{{ messeage }}</p>
+    </div>
 </template>
 
-<style>
-.greeting {
+<style lang="scss">
+/* .greeting {
     color: red;
     font-weight: bold;
 }
 .additional {
     font-weight: bold;
+} */
+
+.block {
+  .greeting {
+    color: blue;
+  }
+  .message {
+    color: red;
+  }
 }
 </style>
