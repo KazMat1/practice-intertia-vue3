5fc430fc5a8c621ee7423781445ce78fe13cb096
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-26 00:15:55 +0900

    Add: 子から親へイベントもなしに値を渡すことは出来ない

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -3,14 +3,13 @@ import { Head } from '@inertiajs/vue3';
 import Navigation from '@/Pages/Components/Navigation/Navigation.vue';
 
 // const props = defineProps({
-//     // title: String,
-//     // heading: String
+//     title: String,
+//     heading: String
 // })
 </script>
 
 <template>
     <Head />
     <Navigation />
-    <!-- <h2 class="heading">{{ props.heading }}</h2> -->
     <slot />
 </template>
