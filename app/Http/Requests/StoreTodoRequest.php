13d5bfa9ff5c66d07f4847e2aa3e694ca7af44a0
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-27 17:27:55 +0900

    Add: バリデーションメッセージの翻訳

--- app/Http/Requests/StoreTodoRequest.php
+++ app/Http/Requests/StoreTodoRequest.php
@@ -26,4 +26,28 @@ class StoreTodoRequest extends FormRequest
             'due_date' => 'required|date|after_or_equal:today',
         ];
     }
+
+    /**
+     * :attributeを翻訳して定義
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'title' => 'タイトル',
+            'due_date' => '締切日',
+        ];
+    }
+
+    /**
+     * 定義されているバリデーションルールのエラーメッセージを翻訳
+     * @return array<string>
+     */
+    public function messages(): array
+    {
+        return [
+            'due_date.after_or_equal' => ':attributeには本日以降の日付を入力してください。',
+        ];
+    }
 }
